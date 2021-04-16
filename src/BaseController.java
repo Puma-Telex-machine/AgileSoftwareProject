3f40c078ef507ec7efb5dc1c0f3579623fdd701c
Joel Olausson <Joel.olaus@gmail.com>
2021-04-16 16:59:12 +0200

    javaFx

---
+++ src/BaseController.java
@@ -0,0 +1,6 @@
+public class BaseController {
+
+    public BaseController(){
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,38 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("view_fxml/imat.fxml"));
+
+
+        Parent root = FXMLLoader.load(getClass().getResource("Base.fxml"));
+        Scene scene = new Scene(root, 800, 500);
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(new BaseController());
+
+        stage.setTitle("UML 2000");
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 }
