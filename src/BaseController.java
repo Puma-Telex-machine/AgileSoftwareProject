6ef586f3661347c855ccd2983df7c75b48a0dce8
Joel Olausson <Joel.olaus@gmail.com>
2021-04-16 17:03:39 +0200

    added skeleton

--- src/BaseController.java
+++ src/BaseController.java
@@ -1,6 +1,22 @@
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class BaseController {
+
+    @FXML
+    private Label boxLabel;
 
     public BaseController(){
 
+    }
+
+    @FXML
+    private void buttonPressed(Event e){
+        //todo Notify backend
+    }
+    public void addBox(){
+        //todo accually add box
+        boxLabel.setText("box added");
     }
 }
