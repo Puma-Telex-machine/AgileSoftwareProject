cf3305160a24fec924bbb598e1764000e798e5cf
Joel Olausson <Joel.olaus@gmail.com>
2021-04-20 16:18:01 +0200

    drag drop functional, boxes added

--- src/viewmodel/BaseController.java
+++ src/viewmodel/BaseController.java
@@ -3,10 +3,8 @@ package viewmodel;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.input.DragEvent;
-import javafx.scene.input.Dragboard;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.input.TransferMode;
+import javafx.scene.layout.AnchorPane;
 import model.Box;
 import model.Model;
 import model.Observer;
@@ -23,11 +21,14 @@ public class BaseController implements Observer{
     @FXML
     private Label source;
     @FXML
-    private Label target;
+    AnchorPane pane;
 
     Model model;
 
+    BoxController box;
+
     public BaseController() {
+
         model = Model.getModel();
         model.addObserver(this);
     }
@@ -38,9 +39,11 @@ public class BaseController implements Observer{
     }
 
     @Override
-    public void addBox(Box box){
-        //todo accually add box
+    public void addBox(Box b){
+        box = new BoxController(b);
+        pane.getChildren().add(this.box);
         boxLabel.setText("box added");
+
     }
     @FXML
     public void contextmenu(){
@@ -48,23 +51,16 @@ public class BaseController implements Observer{
         context.setText("contextMenu opened");
     }
 
-    double offset=0;
-    boolean pressed=false;
-    @FXML
-    public void handleDragStart(MouseEvent event){
-
-    }
     @FXML
     public void handleDragLabel(MouseEvent event){
         source.setText("dragging");
 
         source.setLayoutX(source.getLayoutX()+event.getX()-25);
         source.setLayoutY(source.getLayoutY()+event.getY());
-        System.out.println(offset);
         event.consume();
     }
     public void handleDragOver(MouseEvent event){
-        source.setText("Drag done");
+        source.setText("Drag test");
         event.consume();
     }
 }
