2c500e937ef399952da3c38515f4e394e119b8f5
FHanberg <filhanb@student.chalmers.se>
2021-04-20 18:11:25 +0200

    Preliminary setup for Attribute

--- src/model/Attribute.java
+++ src/model/Attribute.java
@@ -1,4 +1,48 @@
 package model;
 
 public class Attribute {
+
+    String name;
+    Changeable changeable;
+    Visibility visibility;
+
+    enum Visibility{
+        PRIVATE,
+        PACKAGE_PRIVATE,
+        PROTECTED,
+        PUBLIC
+    }
+
+    enum Type{ //Probably requires non-enum solution
+        INT
+    }
+
+    enum Changeable{
+        NON_STATIC,
+        STATIC,
+        FINAL,
+        STATIC_FINAL
+    }
+
+    Attribute(String name, Changeable changeable, Visibility visibility){
+        this.name = name;
+        this.changeable = changeable;
+        this.visibility = visibility;
+    }
+
+    void SetName(String name){
+        this.name = name;
+    }
+
+    void SetVisibility(Visibility visibility){
+        this.visibility = visibility;
+    }
+
+    void SetChangeable(Changeable changeable){
+        this.changeable = changeable;
+    }
+
+
+
+
 }
