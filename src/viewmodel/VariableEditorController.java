fb8cba1bd4a81f138ac2333eaeffd42df037b246
Emil Ekroth <emil@ekroth.se>
2021-04-20 13:33:53 +0200

    Started the controller for the variable editor
    The controller isn't finished yet

---
+++ src/viewmodel/VariableEditorController.java
@@ -0,0 +1,31 @@
+package viewmodel;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class VariableEditorController {
+
+
+    @FXML
+    private TextField nameField; //The text field for the name of the variable
+
+    @FXML
+    private ComboBox accessComboBox; //The combo box for choosing the access of the variable
+
+    @FXML
+    private TextField typeField; //The text field for the type of the variable
+
+    @FXML
+    private void deleteVariable() //This method is called when the player wants to delete the variable
+    {
+
+    }
+
+    @FXML
+    private void confirmVariable() //This method is called when the player wants to confirm and stop editing this variable
+    {
+
+    }
+}
