9fb41f04b02b2437a323bbc2015dd4f027638993
Madeleine Xia <madeleine.xi@hotmail.com>
2021-04-21 09:54:01 +0200

    Controllers for the icons

--- src/viewmodel/Left_menuController.java
+++ src/viewmodel/Left_menuController.java
@@ -1,55 +1,65 @@
 package viewmodel;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
-import javafx.scene.shape.Line;
-import model.Box;
+import javafx.scene.layout.Pane;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.swing.Icon;
 
-public class Left_menuController {
+public class Left_menuController extends AnchorPane {
 
-    @FXML AnchorPane base_menu;
-    @FXML AnchorPane top_anchorpane;
-    @FXML AnchorPane middle_anchorpane;
-    @FXML AnchorPane left_anchorpane;
+    @FXML
+    AnchorPane base_menu, top_anchorpane, middle_anchorpane, left_anchorpane;
+    @FXML
+    Button button1, button2, button3, button4, button5, button6;
+    @FXML
+    Icon icon1, icon2, icon3, icon4, icon5, icon6;
+    @FXML 
+    Pane pane1, pane2, pane3, pane4, pane5, pane6;
 
-    @FXML Button button1;
-    @FXML Button button2;
-    @FXML Button button3;
-    @FXML Button button4;
-    @FXML Button button5;
-    @FXML Button button6;
+    @FXML
+    private void handleButtonAction(ActionEvent event){
+        if(event.getSource() == button1) {
+            pane1.toFront();
+        }
+        else if (event.getSource()== button2) {
+            pane2.toFront();
+        }
+        else if (event.getSource()== button3) {
+            pane3.toFront();
+        }
+        else if (event.getSource()== button4) {
+            pane4.toFront();
+        }
+        else if (event.getSource()== button5) {
+            pane5.toFront();
+        }
+        else if (event.getSource()== button6) {
+            pane6.toFront();
+        }
+        
 
-    @FXML Icon icon1;
-    @FXML Icon icon2;
-    @FXML Icon icon3;
-    @FXML Icon icon4;
-    @FXML Icon icon5;
-    @FXML Icon icon6;
-
-    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Left_menu.fxml"));
-    //fxmlLoader.setRoot(this);
-    //fxmlLoader.setController(this);
-
-    try {
-        fxmlLoader.load();
-    } catch (IOException exception) {
-        throw new RuntimeException(exception);
     }
 
-    
+
+    public Left_menuController() {
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                getClass().getResource("/view/Left_menu.fxml"));
+
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+
+    }
+
 }
