cb0e570c856a30b1a255a7297a9d222af5040d91
Emil Ekroth <emil@ekroth.se>
2021-04-21 14:45:08 +0200

    Method controller start
    Method controller is not finished

---
+++ src/viewmodel/MethodEditorController.java
@@ -0,0 +1,21 @@
+package viewmodel;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.util.List;
+
+public class MethodEditorController {
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private ComboBox accessComboBox;
+
+    @FXML
+    private TextField returnTypeField;
+
+}
