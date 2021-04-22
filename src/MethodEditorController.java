ecc7e5fb9cafeaf0b0c376a512c33a7f4da6a94a
Emil Ekroth <emil@ekroth.se>
2021-04-22 17:28:37 +0200

    updated box facade and method editor controller
    The box facade now has the methods that the attribute editors need.

--- src/MethodEditorController.java
+++ src/MethodEditorController.java
@@ -5,9 +5,12 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.fxml.FXMLLoader;
 import model.MethodData;
+import model.boxes.Box;
 import model.boxes.Visibility;
+import model.facades.BoxFacade;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MethodEditorController extends AnchorPane {
@@ -36,28 +39,56 @@ public class MethodEditorController extends AnchorPane {
     @FXML
     private VBox argumentVBox;
 
+    private BoxFacade box;
+
+    private MethodData methodData;
+
     private MethodArgumentEditorController currentEditArgument;
 
     private List<MethodArgumentEditorController> arguments;
 
     public void AddArgument()
     {
+        //Adds the current edit argument to the arguments
+        currentEditArgument.highlightPane.getStyleClass().clear();
+        currentEditArgument.highlightPane.getStyleClass().add("box");
+        currentEditArgument.argumentTypeField.getStyleClass().clear();
+        currentEditArgument.argumentTypeField.getStyleClass().add("highlight");
         currentEditArgument.argumentTypeField.setOnAction(null);
         arguments.add(currentEditArgument);
-        currentEditArgument = new MethodArgumentEditorController(argumentVBox);
+
+        //Creates a new current edit argument
+        currentEditArgument = new MethodArgumentEditorController();
+        argumentVBox.getChildren().add(currentEditArgument);
+        currentEditArgument.highlightPane.getStyleClass().clear();
+        currentEditArgument.highlightPane.getStyleClass().add("highlight");
+        currentEditArgument.argumentTypeField.getStyleClass().clear();
+        currentEditArgument.argumentTypeField.getStyleClass().add("box");
         currentEditArgument.argumentTypeField.setOnAction((Action) -> AddArgument());
     }
 
     @FXML
     public void ConfirmMethod()
     {
+        methodData.methodName = nameField.getText();
 
+        //Gets the arguments for the method data
+        String[] argRet = new String[arguments.size()];
+        for (int i = 0; i < argRet.length; i++)
+        {
+            argRet[i] = arguments.get(i).argumentTypeField.getText();
+        }
+        methodData.arguments = argRet;
+
+        Visibility visibility = (Visibility) accessComboBox.getSelectionModel().getSelectedItem();
+
+        //box.EditMethod(methodData); todo
     }
 
     @FXML
     public void DeleteMethod()
     {
-
+        box.DeleteMethod(methodData.methodName);
     }
 
     public void EditMethod()
@@ -67,19 +98,37 @@ public class MethodEditorController extends AnchorPane {
 
     public void EditMethod(MethodData methodData)
     {
-        System.out.println();
-        nameField.setText(methodData.methodName);
-        Visibility[] allVisibility = Visibility.values();
-        accessComboBox.getItems().addAll(allVisibility.toString());
-        accessComboBox.getSelectionModel().select(methodData.visibility);
+        //this.box = box; todo
+        this.methodData = methodData;
 
+        //Resets the arguments array
+        arguments = new ArrayList<MethodArgumentEditorController>(0);
+        argumentVBox.getChildren().setAll();
+
+        //Set name
+        nameField.setText(methodData.methodName);
+
+        //Sets the options for the accessibility combo box
+        Visibility[] allVisibility = Visibility.values();
+        accessComboBox.getItems().addAll(allVisibility);
+
+        //Sets the current visibility
+        accessComboBox.getSelectionModel().select(methodData.visibility.name());
+
+        //Sets the arguments for this method
         for (int i = 0; i < methodData.arguments.length; i++)
         {
-            MethodArgumentEditorController argument = new MethodArgumentEditorController(argumentVBox);
+            MethodArgumentEditorController argument = new MethodArgumentEditorController();
             argument.argumentTypeField.setText(methodData.arguments[i]);
+            argumentVBox.getChildren().add(currentEditArgument);
         }
 
-        currentEditArgument = new MethodArgumentEditorController(argumentVBox);
+        currentEditArgument = new MethodArgumentEditorController();
+        argumentVBox.getChildren().add(currentEditArgument);
+        currentEditArgument.highlightPane.getStyleClass().clear();
+        currentEditArgument.highlightPane.getStyleClass().add("highlight");
+        currentEditArgument.argumentTypeField.getStyleClass().clear();
+        currentEditArgument.argumentTypeField.getStyleClass().add("box");
         currentEditArgument.argumentTypeField.setOnAction((Action) -> AddArgument());
     }
 
--- src/model/facades/BoxFacade.java
+++ src/model/facades/BoxFacade.java
@@ -1,4 +1,34 @@
 package model.facades;
 
+import model.MethodData;
+import model.VariableData;
+
 public interface BoxFacade {
+    /**
+     * Should edit the method if the method already exists.
+     * If the method doesn't exist the method should be added instead.
+     * @param methodData The data for the new method. Should replace the old data.
+     */
+    public void EditMethod(MethodData methodData);
+
+    /**
+     * Should edit the variable if the variable exists.
+     * If the variable doesn't exist the variable should be added instead.
+     * @param variableData The data for the new variable. Should replace the old data.
+     */
+    public void EditVariable(VariableData variableData);
+
+    /**
+     * If a method with this name exists then delete the method.
+     * If the method doesn't exist then do nothing.
+     * @param methodName The name of the method that should be deleted
+     */
+    public void DeleteMethod(String methodName);
+
+    /**
+     * If a variable with this name exists then delete that variable.
+     * If the variable doesn't exist then do nothing.
+     * @param variableName The name of the variable that should be deleted.
+     */
+    public void DeleteVariable(String variableName);
 }
