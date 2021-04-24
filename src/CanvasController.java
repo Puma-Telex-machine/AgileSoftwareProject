8883641a86681e6642a2183d31cf4f91c028c4e0
Emil Ekroth <emil@ekroth.se>
2021-04-24 13:51:59 +0200

    Canvas start

---
+++ src/CanvasController.java
@@ -0,0 +1,22 @@
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
+public class CanvasController {
+    public CanvasController()
+    {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(("view/Canvas")));
+
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+
+}
