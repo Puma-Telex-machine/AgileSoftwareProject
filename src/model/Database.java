7edf94fd31ae3206e3968dbcfdeb8d046090d47f
FHanberg <filhanb@student.chalmers.se>
2021-04-28 15:52:09 +0200

    Implemented file loading functionality

--- src/model/Database.java
+++ src/model/Database.java
@@ -1,15 +1,12 @@
 package model;
 
-import model.boxes.Attribute;
-import model.boxes.Box;
-import model.boxes.Method;
-import model.boxes.Modifier;
+import model.boxes.*;
+import model.boxes.Class;
 
+import java.awt.*;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
+import java.util.List;
 
 /**
  * Class that handles saving-/loading functionality.
@@ -17,14 +14,125 @@ import java.util.Set;
  */
 public class Database {
 
-    public void writeTest(String filename){
+    static public Diagram loadDiagram(String filename){
         try {
-            FileWriter filetime = new FileWriter("diagrams/" + filename + ".uml");
-            filetime.write("this is a test, lads");
-            filetime.close();
-        }catch (IOException e){
+            int boxcount;
+            ArrayList<Box> boxes = new ArrayList<>();
+            File toRead = new File("diagrams/" + filename + ".uml");
+            Scanner scanner = new Scanner(toRead);
+            String[] nextSplit = scanner.nextLine().trim().split("=");
+            if(nextSplit[0].toLowerCase().matches("boxcount"))
+                boxcount = Integer.parseInt(nextSplit[1]); // not used for anything yet
+            while (scanner.hasNextLine()){
+                String next = scanner.nextLine().trim();
+                if(next.startsWith("<BOX>"))
+                    boxes.add(loadBox(scanner, BoxType.BOX));
+            }
+            Diagram result = new Diagram();
+            for (Box box: boxes) {
+                result.addBox(box);
+            }
+            return result;
+        } catch (FileNotFoundException e){
             e.printStackTrace();
+            return new Diagram();
         }
+    }
+
+    static private Box loadBox(Scanner scanner, BoxType type){
+        String name = "";
+        int xpos = 0;
+        int ypos = 0;
+        Visibility visibility = Visibility.PUBLIC;
+        List<Method> methods = new ArrayList<>();
+        List<Attribute> attributes = new ArrayList<>();
+        Set<Modifier> modifiers = new HashSet<>();
+        while (scanner.hasNextLine()){
+            String next = scanner.nextLine().trim();
+            if(next.startsWith("name=")){
+                name = next.split("=")[1];
+            }else if(next.startsWith("xposition=")){
+                xpos = Integer.parseInt(next.split("=")[1]);
+            }else if(next.startsWith("yposition=")){
+                ypos = Integer.parseInt(next.split("=")[1]);
+            }else if(next.startsWith("visibility=")){
+                visibility = Visibility.valueOf(next.split("=")[1]);
+            }else if(next.startsWith("<METHOD>")){
+                methods.add(loadMethod(scanner));
+            }else if(next.startsWith("<ATTRIBUTE>")){
+                attributes.add(loadAttribute(scanner));
+            }else if(next.startsWith("modifier")){
+                modifiers.add(Modifier.valueOf(next.split("=")[1]));
+            }else if(next.startsWith("<!")){
+                switch(type){
+                    case BOX:
+                        Box box = new Box(name, new Point(xpos,ypos));
+                        box.setVisibility(visibility);
+                        box.setMethods(methods);
+                        box.setAttributes(attributes);
+                        box.setModifiers(modifiers);
+                        return box;
+                    case CLASS:
+                    case ABSTRACTCLASS:
+                        Class newClass = new Class(new Point(xpos,ypos), name);
+                        newClass.setVisibility(visibility);
+                        newClass.setMethods(methods);
+                        newClass.setAttributes(attributes);
+                        newClass.setModifiers(modifiers);
+                        return newClass;
+                    case INTERFACE:
+                        Interface newInterface = new Interface(new Point(xpos,ypos),name);
+                        newInterface.setVisibility(visibility);
+                        newInterface.setMethods(methods);
+                        newInterface.setAttributes(attributes);
+                        newInterface.setModifiers(modifiers);
+                        return newInterface;
+                }
+            }
+
+        }
+        return null;
+    }
+
+    static private Method loadMethod(Scanner scanner){
+        String name = "";
+        Visibility visibility = Visibility.PUBLIC;
+        List<Attribute> parameters = new ArrayList<>();
+        Set<Modifier> modifiers = new HashSet<>();
+        while (scanner.hasNextLine()){
+            String next = scanner.nextLine().trim();
+            if(next.startsWith("name=")){
+                name = next.split("=")[1];
+            }else if(next.startsWith("visibility=")){
+                visibility = Visibility.valueOf(next.split("=")[1]);
+            }else if(next.startsWith("<PARAMETER>")){
+                parameters.add(loadAttribute(scanner));
+            }else if(next.startsWith("modifier")){
+                modifiers.add(Modifier.valueOf(next.split("=")[1]));
+            }else if(next.startsWith("<!")){
+                return new Method(name, parameters, modifiers,visibility);
+            }
+        }
+        return null;
+    }
+
+    static private Attribute loadAttribute(Scanner scanner){
+        String name = "";
+        Visibility visibility = Visibility.PRIVATE;
+        Set<Modifier> modifiers = new HashSet<>();
+        while (scanner.hasNextLine()){
+            String next = scanner.nextLine().trim();
+            if(next.startsWith("name=")){
+                name = next.split("=")[1];
+            }else if(next.startsWith("visibility=")){
+                visibility = Visibility.valueOf(next.split("=")[1]);
+            }else if(next.startsWith("modifier")){
+                modifiers.add(Modifier.valueOf(next.split("=")[1]));
+            }else if(next.startsWith("<!")){
+                return new Attribute(name, modifiers, visibility);
+            }
+        }
+        return null;
     }
 
     static public void saveDiagram(Diagram target, String filename) {
