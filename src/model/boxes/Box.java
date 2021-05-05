bdb19986d4566c2f3fbbd757b1d6220295b27049
Lukas <lukas.martinsson@gmail.com>
2021-05-05 15:06:14 +0200

    Refractored Box

--- src/model/boxes/Box.java
+++ src/model/boxes/Box.java
@@ -15,35 +15,37 @@ import java.util.List;
 public class Box {
 
     private String name;
+    private BoxType type;
     private List<Method> methods = new ArrayList<>();
     private List<Attribute> attributes = new ArrayList<>();
     private Set<Modifier> modifiers = new HashSet<>();
     private Visibility visibility = Visibility.PUBLIC;
     private ScaledPoint position;
 
-    public Box(String name, ScaledPoint position) {
+    public Box(String name, ScaledPoint position, BoxType type) {
         this.name = name;
         this.position = position;
-    }
-
-    public BoxType getType(){
-        return BoxType.BOX;
+        this.type = type;
     }
 
     public void setName(String newName){
         name = newName;
     }
 
-    public void setPosition(ScaledPoint newPosition){
-        position = newPosition;
+    public String getName() {
+        return name;
+    }
+
+    public BoxType getType(){
+        return type;
+    }
+
+    public void setType(BoxType boxtype){
+        type = boxtype;
     }
 
     public void setMethods(List<Method> newMethods){
         methods = newMethods;
-    }
-
-    public void setAttributes(List<Attribute> newAttributes){
-        attributes = newAttributes;
     }
 
     public Method getMethod(int position){
@@ -52,31 +54,8 @@ public class Box {
         return null;
     }
 
-    public Attribute getAttribute(int position){
-        if(position < attributes.size() && position >= 0)
-            return attributes.get(position);
-        return null;
-    }
-
-    public void setModifiers(Set<Modifier> modifiers){
-        this.modifiers = modifiers;
-    }
-
-    public void addModifier(Modifier modifier){
-        modifiers.add(modifier);
-    }
-
-    public void removeModifier(Modifier modifier){
-        modifiers.remove(modifier);
-    }
-
-    public void setVisibility(Visibility visibility){
-        this.visibility = visibility;
-    }
-
-    public String getName() {
-        return name;
-    }
+    public List<Method> getMethods(){
+        return methods;}
 
     public void editMethod(MethodData methodData) {
         boolean exists = false;
@@ -93,7 +72,32 @@ public class Box {
             methods.add(new Method(methodData));
         }
     }
+    
+    public void deleteMethod(String methodName) {
+        int counter = 0;
+        for (Method method: methods) {
+            if(methodName == method.getName()) {
+                methods.remove(counter);
+                break;
+            }
+            counter++;
+        }
+    }
 
+    public void setAttributes(List<Attribute> newAttributes){
+        attributes = newAttributes;
+    }
+
+    public Attribute getAttribute(int position){
+        if(position < attributes.size() && position >= 0)
+            return attributes.get(position);
+        return null;
+    }
+
+    public List<Attribute> getAttributes(){
+        return attributes;}
+
+    
     public void editVariable(VariableData variableData) {
         boolean exists = false;
         for (Attribute attribute: attributes) {
@@ -109,18 +113,8 @@ public class Box {
         }
     }
 
-    public void deleteMethod(String methodName) {
-        int counter = 0;
-        for (Method method: methods) {
-            if(methodName == method.getName()) {
-                methods.remove(counter);
-                break;
-            }
-            counter++;
-        }
-    }
-
     public void deleteVariable(String variableName) {
+        // TODO: deleteAttribute?
         int counter = 0;
         for (Attribute attribute: attributes) {
             if(variableName == attribute.getName()) {
@@ -131,19 +125,55 @@ public class Box {
         }
     }
 
-    public ScaledPoint getPosition(){return position; }
+    public void setModifiers(Set<Modifier> modifiers){
+        this.modifiers = modifiers;
+    }
 
-    public List<Method> getMethods(){return methods;}
+    public void addModifier(Modifier modifier){
+        modifiers.add(modifier);
+    }
 
-    public List<Attribute> getAttributes(){return attributes;}
+    public Set<Modifier> getModifiers(){
+        return modifiers;}
 
-    public Set<Modifier> getModifiers(){return modifiers;}
+    public void removeModifier(Modifier modifier){
+        // TODO: deleteModifier? (consistency)
+        modifiers.remove(modifier);
+    }
+
+    public void setVisibility(Visibility visibility){
+        this.visibility = visibility;
+    }
 
     public Visibility getVisibility(){
         return visibility;
     }
 
-    public int getHeight() {
-        return 0;
+    public void setPosition(ScaledPoint newPosition){
+        position = newPosition;
     }
-}
+
+    public ScaledPoint getPosition(){
+        return position; }
+
+    public int getHeight() {
+        int count = getMethods().size() + getAttributes().size();
+        int symbolheight = 10;
+        return (count)*symbolheight;
+    }
+    public int getWidth() {
+        ArrayList<Integer> longest = new ArrayList<>();
+        longest.add(getName().length());
+        ArrayList<String> names = new ArrayList<>();
+        for (int i = 0; i < getMethods().size(); i++) {
+            names.add(getMethods().get(i).getName());
+        }
+        for (int i = 0; i < getAttributes().size(); i++) {
+            names.add(getAttributes().get(i).getName());
+        } 
+        for (int i = 0; i < names.size(); i++) {
+            longest.add(names.get(i).length());
+        }
+        return Collections.max(longest);
+    }
+}  
